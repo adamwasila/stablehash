6ee7f2447ffd3ecc11d26cc3c7763fb3409ede86
Adam Wasila <adam.wasila@gmail.com>
2017-05-31 00:37:35 +0200

    Make static factory methods usable (callable from outside of the package)

--- stablehash-core/src/main/java/org/wasila/stablehash/StableHashStaticFactory.java
+++ stablehash-core/src/main/java/org/wasila/stablehash/StableHashStaticFactory.java
@@ -8,19 +8,19 @@ import java.util.Map;
 
 public class StableHashStaticFactory {
 
-    static <N> StableHash<N> newConsistentHash(Collection<N> nodes) {
-        return new ConsistentHash<N>(nodes);
+    public static <N> StableHash<N> newConsistentHash(Collection<N> nodes) {
+        return new ConsistentHash<>(nodes);
     }
 
-    static <N> StableHash<N> newConsistentHash(Map<N, Integer> weightedNodes) {
-        return new ConsistentHash<N>(weightedNodes);
+    public static <N> StableHash<N> newConsistentHash(Map<N, Integer> weightedNodes) {
+        return new ConsistentHash<>(weightedNodes);
     }
 
-    static <N> StableHash<N> newRendezvousHash(Collection<N> nodes) {
-        return new RendezvousHash<N>(nodes);
+    public static <N> StableHash<N> newRendezvousHash(Collection<N> nodes) {
+        return new RendezvousHash<>(nodes);
     }
 
-    static <N> StableHash<N> newRendezvousHash(Map<N, Integer> weightedNodes) {
+    public static <N> StableHash<N> newRendezvousHash(Map<N, Integer> weightedNodes) {
         return new RendezvousHash<>(weightedNodes);
     }
 
